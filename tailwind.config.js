676d8a7e19de943ff8ea9487af4bd83b12383a0b
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2023-05-11 20:08:29 +0700

    build: update tailwind config and global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
@@ -8,11 +8,40 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
-
     theme: {
+        container: {
+            center: true,
+            padding: '1rem',
+            screens: {
+                sm: '100%',
+                md: '900px',
+                lg: '900px',
+                xl: '900px',
+            },
+        },
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Inter', ...fontFamily.sans],
+            },
+            screens: {
+                sm: '600px',
+                md: '900px',
+                lg: '1200px',
+            },
+            keyframes: {
+                wave: {
+                    '0%': { transform: 'rotate(0deg)' },
+                    '10%': { transform: 'rotate(14deg)' },
+                    '20%': { transform: 'rotate(-8deg)' },
+                    '30%': { transform: 'rotate(14deg)' },
+                    '40%': { transform: 'rotate(-4deg)' },
+                    '50%': { transform: 'rotate(10deg)' },
+                    '60%': { transform: 'rotate(0deg)' },
+                    '100%': { transform: 'rotate(0deg)' },
+                },
+            },
+            animation: {
+                wave: 'wave 2.5s ease-in-out infinite',
             },
         },
     },
